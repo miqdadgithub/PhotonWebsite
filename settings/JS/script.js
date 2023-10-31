293a2d6f94d3c4f1350b3d048b902c76ecd98d07
Almiqdad Yahya <95632674+miqdadgithub@users.noreply.github.com>
2023-10-31 18:54:09 +0300

    Update script.js

--- settings/JS/script.js
+++ settings/JS/script.js
@@ -5,7 +5,7 @@ const elemToggleFunc = function (elem) {
   elem.classList.toggle("active");
 };
 
-/** Header in place */
+/** Header in place & go top*/
 const header = document.querySelector("[data-header]");
 const goTopBtn = document.querySelector("[data-go-top]");
 
@@ -29,31 +29,29 @@ navToggleBtn.addEventListener("click", function () {
   elemToggleFunc(document.body);
 });
 
-
 /** Counter  */
 // Set the date we're counting down to
 var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
 
 // Update the count down every 1 second
-var x = setInterval(function() {
-
+var x = setInterval(function () {
   // Get today's date and time
   var now = new Date().getTime();
-    
+
   // Find the distance between now and the count down date
   var distance = countDownDate - now;
-    
+
   // Time calculations for days, hours, minutes and seconds
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-    
-  // If the count down is over, write some text 
+  document.getElementById("demo").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+  // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
     document.getElementById("demo").innerHTML = "EXPIRED";
